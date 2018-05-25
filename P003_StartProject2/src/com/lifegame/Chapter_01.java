fc0024a54db8d1c343491853996d264c96c823de
PashaSelyk <PashaSelyk@yandex.ua>
2018-05-25 15:54:56 +0200

    Greate new Activity 'Chapter_01' and new layout

---
+++ P003_StartProject2/src/com/lifegame/Chapter_01.java
@@ -0,0 +1,22 @@
+package com.lifegame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Chapter_01 extends Activity   {
+
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chapter_01);
+    }
+}
--- P003_StartProject2/src/com/lifegame/NewGameActivity.java
+++ P003_StartProject2/src/com/lifegame/NewGameActivity.java
@@ -1,16 +1,54 @@
 package com.lifegame;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Button;
+import com.example.startproject.R.string;
 
-public class NewGameActivity extends Activity  {
-	
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NewGameActivity extends Activity implements OnClickListener {
+	TextView checkedTextView1;
+	Button okButton;
+	EditText PlayerNameEditText;
 
  
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game_layout);
+        init();
+        checkedTextView1 = (TextView) findViewById(R.id.checkedTextView1);
+        PlayerNameEditText = (EditText) findViewById(R.id.PlayerNameEditText);
+  		
     }  
+    
+    private void init() {
+    	okButton = (Button) findViewById(R.id.okButton);
+    	okButton.setOnClickListener(this);
+    	
+    }
+    
+    public void charSequence(View v) {
+    	
+		String st = PlayerNameEditText.getText().toString();
+    	checkedTextView1.setText(st);
+    	PlayerNameEditText.setText("");
+    }
+    
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.okButton:
+            Intent intent = new Intent(this, Chapter_01.class);
+            startActivity(intent);
+            break;
+        default:
+            break;
+   }
+}
+    
 }
